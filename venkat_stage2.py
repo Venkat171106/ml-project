4e251778fa8a9bd71d44d11d6a22ec014602ac68
venkat <krvenkat950@gmail.com>
2026-02-27 23:03:26 +0530

    first commit

---
+++ venkat_stage2.py
@@ -0,0 +1,36 @@
+# take input
+a, b, op = input("Enter: ").split(", ")
+
+a = int(a)
+b = int(b)
+
+# calculation
+if op == '+':
+    result = a + b
+
+elif op == '-':
+    result = a - b
+
+elif op == '*':
+    result = a * b
+
+elif op == '/':
+    if b == 0:
+        print("Error: Division by zero")
+        exit()
+    result = a / b
+
+else:
+    print("Invalid operator")
+    exit()
+
+print("Result =", result)
+
+if result > 0:
+    print("Positive")
+
+elif result < 0:
+    print("Negative")
+
+else:
+    print("Zero")
